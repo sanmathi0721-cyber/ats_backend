4f66209d0436838755ba71582ca546bc062d61ac
sanmathi0721-cyber <sanmathi0721@gmail.com>
2025-10-04 20:12:26 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def extract_text_from_pdf(file_path: str) -> str:
+    from PyPDF2 import PdfReader
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text
